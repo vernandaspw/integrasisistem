fc6d7e6f49da907a8f86bc0717ff87be4131caf2
vernandaspw <vernandaspw@gmail.com>
2021-01-06 18:39:06 +0700

    et
    eg

--- view/front/home.php
+++ view/front/home.php
@@ -1,17 +1,34 @@
-<div class="container bg- warning" style="margin-top:150px;">
+<div class="container bg- warning" style="margin-top:35px;">
     <div class="row">
         <div class="col-md-6 ">
-            <h5><b>Aplikasi mahasiswa yang ada dikelas integrasi sistem senin malam.</b></h5>
+            <h5><b>Aplikasi mahasiswa kelas integrasi sistem senin malam.</b></h5>
             <br>
-            <b>Akses dosen</b> : Melihat semua daftar mahasiswa, mengedit data dan menghapus data mahasiswa.
+            <b>Akses dosen</b> :
+            <ul>
+                <li>Melihat semua daftar mahasiswa dikelas</li>
+                <li>Mengedit data mahasiswa</li>
+                <li>Menghapus akun mahasiswa</li>
+                <li>Menerima/menolak pendaftaran akun mahasiswa</li>
+                <li>menerima/menolak absensi mahasiswa</li>
+                <li>Melihat laporan absen harian mahasiswa</li>
+            </ul>
             <br>
-            <b>Akses mahasiswa</b> : Melihat semua mahasiswa dikelas integrasis sistem senin malam.
+            <b>Akses mahasiswa</b> :
+            <ul>
+                <li>Melihat semua daftar mahasiswa dikelas</li>
+                <li>Mengajukan Absen harian</li>
+                <li>Melihat laporan absensimu sendiri</li>
+            </ul>
             <br>
+            <b>Tools:</b>
+            <ul>
+                <li>PHP, Bootstrap 5, Google Two Authenticator</li>
+            </ul>
             <br>
             <b class="text-primary">Create By : kelompok 4</b>
         </div>
         <div class="col-md-6">
-            <center> <img src="AI.png" alt="" style="width:300px"></center>
+            <center> <img src="AI.png" alt="" style="width:300px; margin-top:100px;""></center>
 
         </div>
     </div>
